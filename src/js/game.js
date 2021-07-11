e28647b12abf9a95ae98e1e2e3e85d6f1bf83a3a
carbonxx <vishnunv142000@gmail.com>
2021-07-12 00:27:04 +0530

    phaser 3

--- src/js/game.js
+++ src/js/game.js
@@ -1,157 +1,16 @@
 // CREATE NEW GAME OBJECT
 var game = new Phaser.Game(1200, 675, Phaser.AUTO, "phaser-example", {
-	preload: preload,
-	create: create,
-	update: update,
-	render: render,
+  preload: preload,
+  create: create,
+  update: update,
+  render: render,
 });
 
 //////////////////////////////////////////// PRELOAD ////////////////////////////////////////////
 
 function preload() {
-	game.load.image("ball", "./assets/bullet.png"); // SNAKE PARTS BUT ITS CALLED BALL, WHY? ASK @CARRBONXX
-	game.load.image("bg", "./assets/background.png"); // BACKGROUND
-	game.load.spritesheet("ghost", "./assets/ghost-sprite.png", 75, 75); // GHOST
-	game.load.spritesheet("candyBullet", "./assets/candy-sprite.png", 22, 22); // CANDY - BULLETS
-}
-
-///// SOME VARIABLES THAT WE WILL NEED LATER
-// SNAKE 
-var snakeHead; //head of snake sprite
-var snakeSection = new Array(); //array of sprites that make the snake body sections
-var snakePath = new Array(); //arrary of positions(points) that have to be stored for the path the sections follow
-var numSnakeSections = 30; //number of snake body sections
-var snakeSpacer = 2; //parameter that sets the spacing between sections
-var angularSpeed = 200; //angular speed of snake.
-
-// CANDY BULLETS
-var fireRate = 100;
-var nextFire = 0;
-
-// MAKES SNAKE GO BRRRRRR
-var moveSnakeForward = () => {
-	snakeHead.body.velocity.copyFrom(
-		game.physics.arcade.velocityFromAngle(snakeHead.angle, 300)
-	);
-
-	// Everytime the snake head moves, insert the new location at the start of the array,
-	// and knock the last position off the end
-
-	var part = snakePath.pop();
-
-	part.setTo(snakeHead.x, snakeHead.y);
-
-	snakePath.unshift(part);
-
-	for (var i = 1; i <= numSnakeSections - 1; i++) {
-		snakeSection[i].x = snakePath[i * snakeSpacer].x;
-		snakeSection[i].y = snakePath[i * snakeSpacer].y;
-	}
-}
-
-/////////////////////////////////////////////// CREATE ////////////////////////////////////////////
-
-function create() {
-	
-	// SET GAME PHYSICS
-	game.physics.startSystem(Phaser.Physics.ARCADE);
-	game.physics.startSystem(Phaser.Physics.P2JS);
-
-	var group = game.add.group();
-
-	// ADD SPRITES
-	game.add.sprite(0, 0, "bg");
-	game.world.setBounds(0, 0, 800, 600);
-
-	// CREATE INPUT JEYS FOR THE GAME
-	cursors = game.input.keyboard.createCursorKeys(); // ARROW KEYS
-
-	///// CREATE SPRITES
-	// - GHOST SPRITE
-	ghost = game.add.sprite(200, 200, "ghost");
-	ghost.anchor.setTo(0.5, 0.5);
-	
-	// - CANDY BULLETS
-	// candyBullets = game.add.sprite(400, 400, "candyBullet");
-	candyBullets = game.add.group();
-	candyBullets.createMultiple(50, 'bullet');
-	// candyBullet.anchor.setTo(0.5, 0.5);
-
-	// - SNAKE SPRITE
-	snakeHead = game.add.sprite(400, 300, "ball");
-	snakeHead.anchor.setTo(0.5, 0.5);
-
-	///// ENABLE PHYSICS FOR SPRITES
-	// - GHOST PHYSICS
-	game.physics.p2.enable(ghost);
-	// NO CLUE WHAT THIS DOES BUT IT HAS TO BE HERE (GHOST)
-	ghost.body.setZeroDamping();
-	ghost.body.fixedRotation = true;
-
-	// - CANDY BULLET PHYSICS
-	candyBullets.enableBody = true;
-    candyBullets.physicsBodyType = Phaser.Physics.ARCADE;
-	candyBullets.setAll('checkWorldBounds', true);
-    candyBullets.setAll('outOfBoundsKill', true);
-	
-	// - SNAKE PHYSICS
-	game.physics.enable(snakeHead, Phaser.Physics.ARCADE);
-
-	//  INIT SNAKE SECTION ARRAY
-	for (var i = 1; i <= numSnakeSections - 1; i++) {
-		snakeSection[i] = game.add.sprite(400, 300, "ball");
-		snakeSection[i].anchor.setTo(0.5, 0.5);
-	}
-
-	//  INIT SNAKE PATH ARRAY
-	for (var i = 0; i <= numSnakeSections * snakeSpacer; i++) {
-		snakePath[i] = new Phaser.Point(400, 300);
-	}
-
-	// STOPS THE SNAKE FROM GOING OUT
-	snakeHead.body.collideWorldBounds = true;
-
-
-	///// ANIMATIONS
-	// - GHOST
-	ghost.animations.add('walkRight', [0,1], 5, true);
-	ghost.animations.add('walkLeft', [2,3], 5, true);
-	
-	// - CANDY BULLET
-	// candyBullets.animations.add('spin', [0,1], 5, true);
-
-}
-
-/////////////////////////////////////////////// UPDATE ////////////////////////////////////////////
-
-function update() {
-
-	ghost.animations.play('walkLeft');
-	// candyBullets.animations.play('spin');
-
-	ghost.body.setZeroVelocity();
-
-	snakeHead.body.velocity.setTo(0, 0);
-	snakeHead.body.angularVelocity = 0;
-
-	if (cursors.up.isDown) {
-		moveSnakeForward();
-	}
-
-	if (cursors.left.isDown) {
-		snakeHead.body.angularVelocity = -(angularSpeed);
-		moveSnakeForward();
-		// ghost.body.moveLeft(400);
-
-
-	} else if (cursors.right.isDown) {
-		snakeHead.body.angularVelocity = angularSpeed;
-		moveSnakeForward();
-
-		// ghost.body.moveRight(400);
-	}
-}
-
-function render() {
-	game.debug.spriteInfo(snakeHead, 32, 32);
+  game.load.image("ball", "./assets/bullet.png"); // SNAKE PARTS BUT ITS CALLED BALL, WHY? ASK @CARRBONXX
+  game.load.image("bg", "./assets/background.png"); // BACKGROUND
+  game.load.spritesheet("ghost", "./assets/ghost-sprite.png", 75, 75); // GHOST
+  game.load.spritesheet("candyBullet", "./assets/candy-sprite.png", 22, 22); // CANDY - BULLETS
 }
