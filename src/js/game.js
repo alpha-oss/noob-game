6b3be3ffe740a87533c3b8024f11d99a9cc3dd83
carbonxx <vishnunv142000@gmail.com>
2021-07-13 00:26:04 +0530

    :P

--- src/js/game.js
+++ src/js/game.js
@@ -1,23 +1,3 @@
-class SimpleGame {
-  constructor() {
-    this.game = new Phaser.Game(800, 600, Phaser.AUTO, "content", {
-      preload: this.preload,
-      create: this.create,
-    });
-  }
-
-  //game:Phaser.Game;
-
-  preload() {
-    this.load.image("logo", "/assets/shit.png");
-  }
-
-  create() {
-    var logo = this.add.sprite(this.world.centerX, this.world.centerY, "logo");
-    logo.anchor.setTo(0.5, 0.5);
-  }
-}
-
-window.onload = () => {
-  var game = new SimpleGame();
+window.onload = function () {
+  var game = new Phaser.Game();
 };
